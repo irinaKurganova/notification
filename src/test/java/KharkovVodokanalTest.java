d3abad826b6b645866ec9325c2100f7d6c382d52
irina <iriska12sweet2009@rambler.ru>
2018-10-11 23:04:59 +0300

    code refactoring of pom.xml and fixed test a litlle bit

--- src/test/java/KharkovVodokanalTest.java
+++ src/test/java/KharkovVodokanalTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -26,40 +27,32 @@ public class KharkovVodokanalTest
 
 	public static final String WEBDRIVER_CHROME_SYSTEM_PROPERTY = "webdriver.chrome.driver";
 
-	private static WebDriver driver;
+	private  WebDriver driver = null;
 
-	@BeforeClass
-	public static void setupClass() {
+	@Before
+	public  void setupClass() {
 		if (FIREFOX_DRIVER.equals(System.getProperty(BROWSER_PROPERTY))) {
 			driver = new FirefoxDriver();
 		} else if (CHROME_DRIVER.equals(System.getProperty(BROWSER_PROPERTY))) {
-
-			File file = new File(ClassLoader.getSystemClassLoader().getResource(CHROME_DRIVER_NAME).getFile());
-
-			System.setProperty(WEBDRIVER_CHROME_SYSTEM_PROPERTY, file.getAbsolutePath());
+			File resourcesDirectory = new File("selenium/"+CHROME_DRIVER_NAME);
+			System.setProperty(WEBDRIVER_CHROME_SYSTEM_PROPERTY, resourcesDirectory.getAbsolutePath());
 			driver = new ChromeDriver();
 			driver.get("http://vodokanal.kharkov.ua/");
-			driver.get("http://google.com");
+
 		}
 	}
 
-//	@Before
-//	public void setupTest() {
-//		driver = new ChromeDriver();
-//	}
+	@Test
+	public void printVodokanalDataAboutRepair() {
+		driver.get(GOOGLE_PAGE_URL);
+//		kharkovVodokanalPageSteps.clickOnNewsLink();
+		Assert.assertTrue(true);
+	}
 
 	@After
 	public void teardown() {
 		if(driver != null) {
 			driver.quit();
 		}
-	}
-
-	@Test
-	public void printVodokanalDataAboutRepair() {
-//		driver.navigate().to("http://vodokanal.kharkov.ua/");
-//		kharkovVodokanalPageSteps.clickOnNewsLink();
-		System.out.print("! "+ClassLoader.getSystemClassLoader());
-		Assert.assertTrue(true);
 	}
 }
